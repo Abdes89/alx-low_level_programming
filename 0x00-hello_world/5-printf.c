a517db88e6e08ce9e3ddf26f20bb059b417db00d
Abdes89 <abdessamadov@gmail.com>
2023-03-16 17:19:58 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,
  * return: 0 (success)
- */
+*/
  int main (void)
  {
 printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,\n");
