f7b0479c2a2c3758ffbe0f3e98e51f8f668a5353
Abdes89 <abdessamadov@gmail.com>
2023-03-19 15:11:16 -0700

    task_

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#inlude <unistd.h>
+#include <unistd.h>
 /**
  * main - program that prints exactly a sentence
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
